3de1ee8f6e2f15bd05d2909327a1331eb3879412
Scheirer Nicolae (Miki) <58042678+mscheirer@users.noreply.github.com>
2020-04-05 20:54:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lab8_Fibonacci_GitHub
+{
+    public class Fibonacci
+    {
+        public static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, i, numar;
+            Console.Write("Introduceti numarul de elemente din sirul lui Fibonacci: ");
+            numar = int.Parse(Console.ReadLine());
+            Console.Write(n1 + " " + n2 + " ");   
+            for (i = 2; i < numar; i++)   
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+            Console.WriteLine("");
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("Apasa o tasta pentru iesire...");
+        }
+    }
+}
